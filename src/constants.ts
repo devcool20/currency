b69fe9f07ce5edbff369ac9042642294275ba079
Divyanshu Sharma <sharmadivyanshu265@gmail.com>
2024-07-04 18:36:19 +0530

    fc

---
+++ src/constants.ts
@@ -0,0 +1,32 @@
+export const currencyByRupee: Currency[] = [
+    {
+        name: "United States Dollar",
+    value: 1.00,
+    flag: "🇺🇸",
+    symbol: "$"
+    },
+    {
+        name: "Euro",
+    value: 0.85,
+    flag: "🇪🇺",
+    symbol: "€"
+    },
+    {
+        name: "Japanese Yen",
+    value: 110.50,
+    flag: "🇯🇵",
+    symbol: "¥"
+    },
+    {
+        name: "British Pound",
+        value: 0.75,
+        flag: "🇬🇧",
+        symbol: "£"
+    },
+    {
+        name: "Australian Dollar",
+        value: 1.35,
+        flag: "🇦🇺",
+        symbol: "A$"
+    }
+]
